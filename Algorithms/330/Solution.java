849c47ef62b067541b07c4cb106d1872ff2c1b3e
jhdxr <me@jhdxr.com>
2016-05-08 12:06:41 +0800

    Patching Array

--- Algorithms/330/Solution.java
+++ Algorithms/330/Solution.java
@@ -2,31 +2,27 @@ public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.minPatches(new int[]{1, 2, 3, 4, 5}, 10));
-        System.out.println(s.minPatches(new int[]{1, 2, 3, 4, 500}, 1));
-        System.out.println(s.minPatches(new int[]{10}, 10));
         System.out.println(s.minPatches(new int[]{1, 3}, 6));
         System.out.println(s.minPatches(new int[]{1, 5, 10}, 20));
         System.out.println(s.minPatches(new int[]{1, 2, 2}, 5));
-        System.out.println(s.minPatches(new int[]{}, 200));
+        System.out.println(s.minPatches(new int[]{}, 3));
     }
 
     public int minPatches(int[] nums, int n) {
         int patch = 0;
-        long sum = 0;
-        for (int i : nums) {
-            while (i > sum + 1) {
-                if (sum >= n) return patch;
+        int cur = 1;
+        int index = 0;
+        do {
+            int num;
+            if (index >= nums.length || cur < nums[index]) { //missing number, need patch
+                num = cur;
                 patch++;
-                sum += sum + 1;
+            } else {
+                num = nums[index];
+                index++;
             }
-            if (sum >= n) return patch;
-            sum += i;
-        }
-        while (sum < n) {
-            patch++;
-            sum += sum + 1;
-        }
+            cur += num;
+        } while (cur <= n);
         return patch;
     }
 }
